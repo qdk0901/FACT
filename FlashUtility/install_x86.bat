9e1cba3e4cfa73b5f0a3e651a0d43a50b2922481
unknown <derek@abc.(none)>
2012-01-31 09:06:13 +0800

    添加secusb，adb驱动

--- FlashUtility/install_x86.bat
+++ FlashUtility/install_x86.bat
@@ -1,3 +1,5 @@
+rundll32 syssetup,SetupInfObjectInstallAction DefaultInstall 128 adb_driver\android_winusb.inf
+rundll32 syssetup,SetupInfObjectInstallAction DefaultInstall 128 sec_driver\secusb2.inf
 cd x86
 install-filter install -ac
 pause
